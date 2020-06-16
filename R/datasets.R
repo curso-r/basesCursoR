c78de6c7d7c3d0889b19d0950da4bfa86cb7cca8
Athos Damiani <athos.damiani@gmail.com>
2020-06-16 00:59:06 -0300

    adult dataset

--- R/datasets.R
+++ R/datasets.R
@@ -28,3 +28,32 @@
 #' }
 #' @source \url{https://covid.saude.gov.br/}
 "covid"
+
+
+
+#' Adult Dataset
+#'
+#' Extração feita por Barry Becker do Censo de 1994 dos Estados Unidos.
+#'
+#' Objetivo de predição é determinar se a pessoa ganha acima de 50 mil dólares por ano.
+#'
+#' @format Um data frame com 48842 linhas e 15 variáveis:
+#' \describe{
+#'   \item{age}{}
+#'   \item{workclass}{}
+#'   \item{fnlwgt}{}
+#'   \item{education}{}
+#'   \item{education_num}{}
+#'   \item{marital_status}{}
+#'   \item{occupation}{}
+#'   \item{relationship}{}
+#'   \item{race}{}
+#'   \item{sex}{}
+#'   \item{capital_gain}{}
+#'   \item{capital_loss}{}
+#'   \item{hours_per_week}{}
+#'   \item{native_country}{}
+#'   \item{more_than_50k}{variável resposta com duas categorias: "yes" e "no"}
+#' }
+#' @source \url{http://archive.ics.uci.edu/ml/datasets/Adult}
+"adult"
---
+++ data-raw/adult.R
@@ -0,0 +1,54 @@
+# pacotes
+library(tidymodels)
+library(tidyverse)
+library(DataExplorer)
+library(skimr)
+library(pROC)
+
+#####################################################################################################
+# PASSO 0) CARREGAR AS BASES
+
+# Download dos dados -----------------------------------------------------------------------------
+
+# baixa adult.data se nao existe ainda
+tmp_data <- tempfile("adult.data")
+httr::GET("http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data", httr::write_disk(tmp_data))
+
+# baixa adult.test se nao existe ainda
+tmp_test <- tempfile("adult.test")
+httr::GET("http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test", httr::write_disk(tmp_test))
+
+# baixa adult.names se nao existe ainda
+tmp_names <- tempfile("adult.names")
+httr::GET("http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.names", httr::write_disk(tmp_names))
+
+# Carrega dados ---------------------------------------------------------------------------------------
+
+# prepara os nomes das colunas para colocar no cabecalho
+adult_names <- tibble(name = read_lines(tmp_names)) %>%
+  filter(
+    str_detect(name, "^[^\\|].*:")
+  ) %>%
+  separate(name, c("name", "description"), sep = ":") %>%
+  mutate(
+    name = snakecase::to_snake_case(name)
+  ) %>%
+  add_row(name = "more_than_50k", description = "person earn more than USD 50K per year.")
+
+# treino/teste -------------------------------------------
+adult_train <- read_csv(file = tmp_data, na = c("?", "", "NA"), col_names = adult_names$name) %>%
+  mutate(
+    more_than_50k = if_else(more_than_50k == "<=50K", "no", "yes")
+  )
+adult_test  <- read_csv(file = tmp_test, na = c("?", "", "NA"), col_names = adult_names$name, skip = 1) %>%
+  mutate(
+    more_than_50k = if_else(more_than_50k == "<=50K.", "no", "yes")
+  )
+
+adult <- list(
+  treino = adult_train,
+  teste = adult_test
+)
+
+
+usethis::use_data(adult, overwrite = TRUE)
